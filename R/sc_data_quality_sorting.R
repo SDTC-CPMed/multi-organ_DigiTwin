23bfbbac5cc9d58ef454ad08d8cd2087826329b3
sandralilja <sandra.lilja@liu.se>
2022-04-07 11:35:06 +0200

    update script

--- R/sc_data_quality_sorting.R
+++ R/sc_data_quality_sorting.R
@@ -1,27 +1,37 @@
+# Sandra Lilja
+# R version 4.0.4
+#'
+#' Check the quality of the data and remove outliers
+#' 
+#' @param filename The filename of the csv.gz input file with cells in columns and genes in rows
+#' @param organ The organ as a string (eg., "Joint"). Based on this parameter, outliers will be romoved as manually defined for our data. 
+#' 
+#' @export
+#'
+
 library(Seurat)
-library(dplyr)
-library(Matrix)
-library(R.utils)
-library(patchwork)
+# library(dplyr)
+# library(Matrix)
+# library(R.utils)
+# library(patchwork)
 
 
-rm(list=ls())
-dir.home <- getwd()
-dir.home <- paste(dir.home, '/DGE_data/', sep = '')
-dir.data <- dir.home
-dir.out <- paste(dir.home, 'sorted_DGEs/', sep = '')
-if (dir.exists(dir.out)==FALSE){
-  dir.create(dir.out, recursive = T)
-  print('dir.out was created')
-}
+# rm(list=ls())
+# dir.home <- getwd()
+# # dir.home <- '/data/sanli71/RA-CIA_project/CIA_project'
+# dir.home <- paste(dir.home, '/DGE_data/', sep = '')
+# dir.data <- dir.home
+# dir.out <- paste(dir.home, 'sorted_DGEs/', sep = '')
+# if (dir.exists(dir.out)==FALSE){
+#   dir.create(dir.out, recursive = T)
+#   print('dir.out was created')
+# }
+# files <- list.files(dir.data, pattern = 'txt.gz')
+# organs <- sapply(strsplit(files, '_'), '[[', 1)
 
-projectname <- sapply(strsplit(dir.home, '/'), '[[', 4)
-files <- list.files(dir.data, pattern = 'txt.gz')
-tissues <- sapply(strsplit(files, '_'), '[[', 1)
-
-# tissue <- tissues[1]
-for (tissue in tissues){
-  X <- read.table(paste(dir.data, list.files(dir.data, pattern = tissue), sep = ''), header = T, sep = '\t', row.names = 1)
+sc_data_quality_sorting <- function(filename, organ){
+  # X <- read.table(paste(dir.data, list.files(dir.data, pattern = organ), sep = ''), header = T, sep = '\t', row.names = 1)
+  X <- read.table(filename, header = T, sep = '\t', row.names = 1)
   X[1:5,1:5]
   X_backup <- X
   
@@ -29,32 +39,29 @@ for (tissue in tissues){
   X.seu <- CreateSeuratObject(X, min.cells = ceiling(length(colnames(X))*0.01))
   X.seu[["percent.mito"]] <- PercentageFeatureSet(X.seu, pattern = "^mt-")
   X.seu <- subset(X.seu, subset = nFeature_RNA > 200 & nCount_RNA > 400 & percent.mito < 20)
-
+  
   # VlnPlot(X.seu, features = c("nFeature_RNA", "nCount_RNA", "percent.mito"), ncol = 3)
   # plot1 <- FeatureScatter(X.seu, feature1 = "nCount_RNA", feature2 = "percent.mito")
   # plot2 <- FeatureScatter(X.seu, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
   # plot1 + plot2
   
   ### Sort the data based on results from previous graphs
-  if (tissue == 'Joint'){
-    print(tissue)
-  } else if (tissue == 'Lung'){
-    print(tissue)
+  if (organ == 'Joint'){
+  } else if (organ == 'Lung'){
     X.seu <- subset(X.seu, subset = nCount_RNA < 2000)
-  } else if (tissue %in% c('Spleen')){
-    print(tissue)
+  } else if (organ %in% c('Spleen')){
     X.seu <- subset(X.seu, subset = nCount_RNA < 6000)
-  } else if (tissue %in% c('Muscle', 'Skin')){
-    print(tissue)
+  } else if (organ %in% c('Muscle', 'Skin')){
     X.seu <- subset(X.seu, subset = nCount_RNA < 7000)
   } else {
-    print('tissue specific sorting conditions not defined')
+    print('organ specific sorting conditions not defined')
   }
-
+  
   ### Extract expression matrix
   X <- as.data.frame(as.matrix(GetAssayData(X.seu, slot = "counts")))
   X[1:5,1:5]
-  write.csv(X, paste(dir.out,  paste(tissue, '_sorted_expression_matrix.csv', sep = ''), sep = ''), quote = F)
-
+  # write.csv(X, paste(dir.out,  paste(organ, '_sorted_expression_matrix.csv', sep = ''), sep = ''), quote = F)
+  write.csv(X, paste('data/sorted_DGEs/', organ, '_sorted_expression_matrix.csv', sep = ''), quote = F)
+  
 }
 
